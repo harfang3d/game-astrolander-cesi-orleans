662ac9fcaf55eb2957b11dfa7c30d34d2e728bcc
Ugo Licatesi <ugo.licatesi@gmail.com>
2022-12-08 14:11:39 +0100

    Create new level
    Added level_1

--- source/levels.py
+++ source/levels.py
@@ -1,3 +1,4 @@
 levels = [
     {"level" :"scenes/levels/level_0.scn", "music": "audio/music/bouncy_optimism.wav", "background": "assets/background.scn"},
+    {"level" :"scenes/levels/level_1.scn", "music": "audio/music/bouncy_optimism.wav", "background": "assets/background.scn"},
 ]
