a0b03186183aa648086a7ec53355e0ab19741cfc
Ugo Licatesi <ugo.licatesi@gmail.com>
2022-12-08 14:29:47 +0100

    update order

--- source/levels.py
+++ source/levels.py
@@ -1,8 +1,8 @@
 levels = [
-    {"level" :"scenes/levels/level_10.scn", "music": "audio/music/bouncy_optimism.wav", "background": "assets/background.scn"},
     {"level" :"scenes/levels/level_0.scn", "music": "audio/music/bouncy_optimism.wav", "background": "assets/background.scn"},
     {"level" :"scenes/levels/level_3.scn", "music": "audio/music/bouncy_optimism.wav", "background": "assets/background.scn"},
     {"level" :"scenes/levels/level_5.scn", "music": "audio/music/bouncy_optimism.wav", "background": "assets/background.scn"},
     {"level" :"scenes/levels/level_6.scn", "music": "audio/music/bouncy_optimism.wav", "background": "assets/background.scn"},
+    {"level" :"scenes/levels/level_10.scn", "music": "audio/music/bouncy_optimism.wav", "background": "assets/background.scn"},
     {"level" :"scenes/levels/level_11.scn", "music": "audio/music/bouncy_optimism.wav", "background": "assets/background.scn"},
 ]
