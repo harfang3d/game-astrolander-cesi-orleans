e16706b7451115d7226eadacf3f29659ffe6f38b
Astrofra <astrofra@gmail.com>
2022-12-08 14:32:04 +0100

    Sort level orders.

--- source/levels.py
+++ source/levels.py
@@ -1,9 +1,9 @@
 levels = [
     {"level" :"scenes/levels/level_0.scn", "music": "audio/music/bouncy_optimism.wav", "background": "assets/background.scn"},
     {"level" :"scenes/levels/level_1.scn", "music": "audio/music/bouncy_optimism.wav", "background": "assets/background.scn"},
-    {"level" :"scenes/levels/level_9.scn", "music": "audio/music/bouncy_optimism.wav", "background": "assets/background.scn"},
-    {"level" :"scenes/levels/level_10.scn", "music": "audio/music/bouncy_optimism.wav", "background": "assets/background.scn"},
     {"level" :"scenes/levels/level_3.scn", "music": "audio/music/bouncy_optimism.wav", "background": "assets/background.scn"},
     {"level" :"scenes/levels/level_6.scn", "music": "audio/music/bouncy_optimism.wav", "background": "assets/background.scn"},
+    {"level" :"scenes/levels/level_9.scn", "music": "audio/music/bouncy_optimism.wav", "background": "assets/background.scn"},
+    {"level" :"scenes/levels/level_10.scn", "music": "audio/music/bouncy_optimism.wav", "background": "assets/background.scn"},
     {"level" :"scenes/levels/level_11.scn", "music": "audio/music/bouncy_optimism.wav", "background": "assets/background.scn"},
 ]
